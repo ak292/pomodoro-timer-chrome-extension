f030349e339b2fc86aea39be2d2e6021e69f0333
Ahmed Keshka <up962500@myport.ac.uk>
2024-09-06 20:02:14 +0300

    added sync storage for saving tasks

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,60 @@
-const addTaskBtn = document.getElementById("add-task-btn");
-addTaskBtn.addEventListener("click", () => addTask());
+let tasks = [];
 
-function addTask() {
-  // create a div
+// load tasks when popup is opened
+chrome.storage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  tasks.forEach((task, index) => {
+    addTask(task, index);
+  });
+});
+
+function saveTasks() {
+  chrome.storage.sync.set({ tasks });
+}
+
+function addTask(value = "", taskNum = tasks.length) {
   const taskRow = document.createElement("div");
+  taskRow.setAttribute("id", `del-${taskNum}`);
 
-  // create the input
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task...";
+  text.value = value;
+  text.addEventListener("change", () => {
+    tasks[taskNum] = text.value;
+    saveTasks();
+    console.log(tasks);
+  });
 
   // create the delete button
   const deleteBtn = document.createElement("input");
   deleteBtn.type = "button";
   deleteBtn.value = "X";
+  deleteBtn.addEventListener("click", () => {
+    const currentIndex = tasks.indexOf(text.value);
+    if (currentIndex !== -1) {
+      tasks.splice(currentIndex, 1);
+      saveTasks();
+    }
 
-  // append them to the div
+    const rowToDelete = document.getElementById(`del-${taskNum}`);
+    rowToDelete.remove();
+    console.log(tasks);
+  });
+
+  // append the input and delete button to the task row
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
 
-  // each row we add is now its own div with an input
-  // and a delete button, then we add to the taskContainer
+  // append the task row to the container
   const taskContainer = document.getElementById("task-container");
   taskContainer.appendChild(taskRow);
 }
+
+// add a new task when the "Add Task" button is clicked
+const addTaskBtn = document.getElementById("add-task-btn");
+addTaskBtn.addEventListener("click", () => {
+  tasks.push("");
+  addTask();
+  saveTasks();
+});
