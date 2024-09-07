5415593fccfa9a887d97f8ec3143b0fd068cb660
Ahmed Keshka <up962500@myport.ac.uk>
2024-09-07 04:31:32 +0300

    fixed timer on popup and added reset timer functionality

---
+++ background.js
@@ -0,0 +1,27 @@
+/* use chrome.alarms so background service worker
+doesnt go inactive, we need it to run constantly
+to increment the timer every 1 second */
+chrome.alarms.create("pomodoroTimer", {
+  periodInMinutes: 1 / 60,
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "pomodoroTimer") {
+    chrome.storage.local.get(["timer", "isRunning"], (res) => {
+      if (res.isRunning) {
+        let timer = res.timer + 1;
+        console.log(timer);
+        chrome.storage.local.set({
+          timer,
+        });
+      }
+    });
+  }
+});
+
+chrome.storage.local.get(["timer", "isRunning"], (res) => {
+  chrome.storage.local.set({
+    timer: "timer" in res ? res.timer : 0,
+    isRunning: "isRunning" in res ? res.isRunning : false,
+  });
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,64 @@
 let tasks = [];
 
+/* the timer increments every second using the
+background service worker, here we just grab
+the value and make use of it for popup */
+function updateTime() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = document.getElementById("time");
+    const minutes = `${25 - Math.ceil(res.timer / 60)}`.padStart(2, "0");
+    let seconds = "00";
+    if (res.timer % 60 != 0) {
+      seconds = 60 - (res.timer % 60);
+    }
+    time.textContent = `${minutes}:${seconds}`;
+  });
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+const startTimerBtn = document.getElementById("start-timer-btn");
+
+// simple function to make sure button text content is consistent
+// whenever user closes the popup and opens it up again
+function checkButton() {
+  chrome.storage.local.get("isRunning", (res) => {
+    if (res.isRunning) {
+      startTimerBtn.textContent = "Pause Timer";
+    }
+  });
+}
+
+// run function everytime user opens popup
+checkButton();
+
+startTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.get("isRunning", (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer";
+      }
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+    }
+  );
+});
+
 // load tasks when popup is opened
 chrome.storage.sync.get(["tasks"], (res) => {
   tasks = res.tasks ? res.tasks : [];
